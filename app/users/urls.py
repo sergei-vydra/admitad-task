40ec22cc70a5561f01bca2b07509f7180b6db97f
Sergei Vydra <35927520+sergei-vydra@users.noreply.github.com>
2024-03-19 13:55:48 +0300

    004: fine-tuning users app (#12)

--- app/users/urls.py
+++ app/users/urls.py
@@ -1,6 +1,17 @@
-from django.urls import include, path
+from django.urls import include, path, re_path
+from .views.users import LogoutView, VerifyEmailView
+from dj_rest_auth.views import PasswordResetConfirmView
 
 urlpatterns = [
+    path("logout/", LogoutView.as_view(), name="rest_logout"),
+    path(
+        "password/reset/confirm/<uidb64>/<token>/", PasswordResetConfirmView.as_view(), name="password_reset_confirm"
+    ),
     path("", include("dj_rest_auth.urls")),
+    re_path(
+        r"^registration/account-confirm-email/(?P<key>[-:\w]+)/$",
+        VerifyEmailView.as_view(),
+        name="account_confirm_email",
+    ),
     path("registration/", include("dj_rest_auth.registration.urls")),
 ]
