d9b1bc150357d46f952bdbde506ebe8457c008e9
sergei.vydra <vydrasergei@gmail.com>
2024-03-20 14:43:11 +0300

    014: add redis cache

--- api/settings/components/cache.py
+++ api/settings/components/cache.py
@@ -1,3 +1,11 @@
 from api import settings
 
 REDIS_URL = settings.env("REDIS_URL")
+
+CACHES = {
+    "default": {
+        "BACKEND": "django.core.cache.backends.redis.RedisCache",
+        "LOCATION": REDIS_URL,
+        "TIMEOUT": 60 * 15,
+    }
+}
